4cb16c4296dbb48802a5ddb773175e40ea6ca458
Afiaqamar <afiaqamar@gmail.com>
2020-06-10 13:32:00 +0500

    app file updated

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Updated by Afia Qamar</p>
+        <p>New comment added</p>
         <a
           className="App-link"
           href="https://reactjs.org"
